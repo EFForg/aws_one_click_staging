7ffb846ca604cd8c4c7ed4b9b4616551877a0b4d
TheNotary <no@email.plz>
2015-11-26 00:38:30 -0800

    bug fixes and touchups

--- lib/aws_one_click_staging.rb
+++ lib/aws_one_click_staging.rb
@@ -10,9 +10,12 @@ module AwsOneClickStaging
     warrior = AwsWarrior.new
   end
 
-  def self.list
+  def self.check
     warrior = AwsWarrior.new # this makes a config file if needed
-    puts "This is aws_one_click_staging, use stage to set things up"
+    puts "This command *would* test that you have the needed "
+    puts "permissions on the buckets and rds instances you named "
+    puts "in your config file... but alas, you're reading the "
+    puts "outputs of a stubbed method..."
   end
 
 end
